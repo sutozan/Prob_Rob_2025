94790a63b560f966a7c3316bab9d479ca8949f68
Suheyla Tozan <suheylatozan02@gmail.com>
2025-10-20 01:41:52 -0400

    Lab 4, A6: Step 1: Adding the Prediction Model

--- prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
+++ prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import math, numpy as np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu, JointState
 from message_filters import Subscriber, ApproximateTimeSynchronizer
@@ -13,8 +14,17 @@ class Odometry_Tracking(Node):
     def __init__(self):
         super().__init__('odometry_tracking')
         self.log = self.get_logger()
-        
-        # Considering hte latest command velocities
+
+        # Define model parameters for linear and angular velocity
+        self.tau_lin = 0.55 #t90 from graph approx
+        self.tau_ang = 0.35 #t90 from graph approx
+        self.G_v = 1.0
+        self.G_w = 1.0
+
+        # Now we initilize the nonlinear state update
+        self.state = np.zeros(5)
+
+        # Initilize - Considering the latest command velocities
         self.u_v = 0.0
         self.u_w = 0.0
         self.last_time = None
@@ -30,7 +40,8 @@ class Odometry_Tracking(Node):
         sync = ApproximateTimeSynchronizer([imu_sub, joint_sub], 10, 0.03)
         sync.registerCallback(self.synced_callback)
 
-        self.log.info("Step 0 - Testing Initialization")
+        self.log.info("Step 1 - Setting up Prediction Model")
+        
 
     def cmd_callback(self, msg):
         self.u_v = msg.linear.x
@@ -60,13 +71,41 @@ class Odometry_Tracking(Node):
         dt = t - self.last_time
         self.last_time = t
 
-        # log
+        # Log before the prediction model
         self.log.info(
             f"Δt={dt:.3f}s  ωr={w_r:.2f}  ωl={w_l:.2f}  "
             f"ωg={w_g:.3f}  u_v={self.u_v:.2f}  u_ω={self.u_w:.2f}"
         )
 
+        # Set up the prediction model
 
+        # Computer the forgetting factor
+        a_v = math.pow(0.1, dt / self.tau_lin)
+        a_w = math.pow(0.1, dt / self.tau_ang)
+
+        # unpack state and control
+        theta, x, y, v, ω = self.state
+        u_v, u_w = self.u_v, self.u_w
+
+        # first-order dynamic response
+        v_pred = a_v * v + self.G_v * (1.0 - a_v) * u_v
+        ω_pred = a_w * ω + self.G_w * (1.0 - a_w) * u_w
+
+        # integrate pose
+        theta_pred = theta + ω_pred * dt
+        x_pred = x + v_pred * math.cos(theta_pred) * dt
+        y_pred = y + v_pred * math.sin(theta_pred) * dt
+
+        # update state
+        self.state = np.array([theta_pred, x_pred, y_pred, v_pred, ω_pred])
+
+        # Log predicted results
+        self.get_logger().info(
+            f"Predicted: x={x_pred:.3f} y={y_pred:.3f} θ={theta_pred:.3f} "
+            f"v={v_pred:.3f} ω={ω_pred:.3f} (a_v={a_v:.3f}, a_w={a_w:.3f})"
+        )
+
+    
     def spin(self):
         rclpy.spin(self)
 
