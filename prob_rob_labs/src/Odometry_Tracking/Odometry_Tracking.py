64f1c3a48756c6bd5e96c2545ef6f68a5f3c7e69
Suheyla Tozan <suheylatozan02@gmail.com>
2025-10-20 04:12:49 -0400

    Lab 4: Assignment 6, Fully working filter

--- prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
+++ prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
@@ -1,10 +1,10 @@
 import rclpy
 from rclpy.node import Node
 import math, numpy as np
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Quaternion
 from sensor_msgs.msg import Imu, JointState
+from nav_msgs.msg import Odometry
 from message_filters import Subscriber, ApproximateTimeSynchronizer
-
 
 # Everything works - commit.
 # Also can run this for teleop -  ros2 run turtlebot3_teleop teleop_keyboard
@@ -44,7 +44,8 @@ class Odometry_Tracking(Node):
         sync = ApproximateTimeSynchronizer([imu_sub, joint_sub], 10, 0.03)
         sync.registerCallback(self.synced_callback)
 
-        self.log.info("Step 2 - Performing EKF Prediction")
+        # Publish EKF Odometry
+        self.odom_pub = self.create_publisher(Odometry, '/ekf_odom', 10)
 
     def forgetting(self, dt):
         a_v = math.pow(0.1, dt / self.tau_lin)
@@ -176,7 +177,40 @@ class Odometry_Tracking(Node):
             f"v={v_next:.3f} w={w_next:.3f} (a_v={a_v:.3f}, a_w={a_w:.3f})"
             f"Update Covariance (Diag only) = {np.diag(self.state_covariance)}"
         )
+        
+        # Publish EKF Odometry
+        odom_msg = Odometry()
+        odom_msg.header.stamp = imu_msg.header.stamp
+        odom_msg.header.frame_id = "odom"
+        odom_msg.child_frame_id = "base_link"
 
+        # Now we publish the position (x,y,z)
+        odom_msg.pose.pose.position.x = self.state[1]
+        odom_msg.pose.pose.position.y = self.state[2]
+        odom_msg.pose.pose.position.z = 0.0
+
+        # Take consideration of the quaternions - publish
+        qz = math.sin(self.state[0]/2.0)
+        qw = math.cos(self.state[0]/2.0)
+        odom_msg.pose.pose.orientation = Quaternion(x =0.0, y=0.0, z=qz, w=qw)
+
+        # Now we have to map 5x5 state to 6x6 expectation
+        # First pose
+        odom_msg.pose.covariance = [0.0] * 36
+        diag_covariances = np.diag(self.state_covariance)
+        odom_msg.pose.covariance[0] = diag_covariances[1]
+        odom_msg.pose.covariance[7] = diag_covariances[2]
+        odom_msg.pose.covariance[35] = diag_covariances[0]
+
+        # Next twist
+        odom_msg.twist.twist.linear.x = self.state[3]
+        odom_msg.twist.twist.angular.z = self.state[4]
+        odom_msg.twist.covariance = [0.0]*36
+        odom_msg.twist.covariance[0] = diag_covariances[3]
+        odom_msg.twist.covariance[35] = diag_covariances[4]
+
+        # Publish everything
+        self.odom_pub.publish(odom_msg)
     
     def spin(self):
         rclpy.spin(self)
