e9e48655136f6c0a59f18b8ababcd63517d2d3a8
Suheyla Tozan <suheylatozan02@gmail.com>
2025-10-20 01:17:37 -0400

    Lab 4, A6: Testing Initilization

--- prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
+++ prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
@@ -1,18 +1,71 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu, JointState
+from message_filters import Subscriber, ApproximateTimeSynchronizer
 
 
-heartbeat_period = 0.1
+# Everything works - commit.
+# Also can run this for teleop -  ros2 run turtlebot3_teleop teleop_keyboard
 
 class Odometry_Tracking(Node):
 
     def __init__(self):
-        super().__init__('Odometry_Tracking')
+        super().__init__('odometry_tracking')
         self.log = self.get_logger()
-        self.timer = self.create_timer(heartbeat_period, self.heartbeat)
+        
+        # Considering hte latest command velocities
+        self.u_v = 0.0
+        self.u_w = 0.0
+        self.last_time = None
 
-    def heartbeat(self):
-        self.log.info('heartbeat')
+        # Subscriber for /cmd_vel
+        self.create_subscription(Twist, '/cmd_vel', self.cmd_callback, 10)
+
+        # Subscribers for measurment system
+        imu_sub = Subscriber(self, Imu, '/imu')
+        joint_sub = Subscriber(self, JointState, '/joint_states')
+
+        # Time synchronizer
+        sync = ApproximateTimeSynchronizer([imu_sub, joint_sub], 10, 0.03)
+        sync.registerCallback(self.synced_callback)
+
+        self.log.info("Step 0 - Testing Initialization")
+
+    def cmd_callback(self, msg):
+        self.u_v = msg.linear.x
+        self.u_w = msg.angular.z
+
+    def synced_callback(self, imu_msg, joint_msg):
+        # Extracting w_g from gyroscope
+        w_g = imu_msg.angular_velocity.z
+
+        # Next we need wheel speeds from joint states
+        w_r = w_l = 0.0
+        for name, vel in zip(joint_msg.name, joint_msg.velocity):
+            if 'wheel_right_joint' in name:
+                w_r = vel
+            elif 'wheel_left_joint' in name:
+                w_l = vel
+        
+        # compute delta t # got 0.034s
+        t = max(
+            imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec * 1e-9,
+            joint_msg.header.stamp.sec + joint_msg.header.stamp.nanosec * 1e-9,
+        )
+
+        if self.last_time is None:
+            self.last_time = t
+            return
+        dt = t - self.last_time
+        self.last_time = t
+
+        # log
+        self.log.info(
+            f"Δt={dt:.3f}s  ωr={w_r:.2f}  ωl={w_l:.2f}  "
+            f"ωg={w_g:.3f}  u_v={self.u_v:.2f}  u_ω={self.u_w:.2f}"
+        )
+
 
     def spin(self):
         rclpy.spin(self)
