97e0701fb73b7556e8eec4a855272e55fbb0eaa5
Suheyla Tozan <suheylatozan02@gmail.com>
2025-10-24 20:37:51 -0400

    Final submission: odometry_tracking, euclidean_distance_determiner, state_extraction nodes

--- prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
+++ prob_rob_labs/src/Odometry_Tracking/Odometry_Tracking.py
@@ -24,6 +24,11 @@ class Odometry_Tracking(Node):
         # Now we initilize the nonlinear state update
         self.state = np.zeros(5)
 
+        # The following were commented out but kept here for testing sake - we wanted to see how it impacted our results if we 
+        # start at a different angle.
+        #self.state = np.array([math.pi / 2, 0.0, 0.0, 0.0, 0.0]) #start at 90 degrees
+        #self.state = np.array([math.radians(45), 0.0, 0.0, 0.0, 0.0]) #start at 45 degrees
+
         # Initialize covariances for update
         self.state_covariance = np.eye(5) * 0.01
         self.input_covariance = np.diag([0.05**2, 0.05**2])
@@ -36,11 +41,14 @@ class Odometry_Tracking(Node):
         # Subscriber for /cmd_vel
         self.create_subscription(Twist, '/cmd_vel', self.cmd_callback, 10)
 
-        # Subscribers for measurment system
+        # Subscribers for measurment system 
+        # Reason why this is differnet than above - we are using these to synchronize the time so its from the message filter class
+        # otherwise we would have the two data come in at different rates 
+        # reference - https://docs.ros.org/en/rolling/p/message_filters/doc/Tutorials/Approximate-Synchronizer-Python.html
         imu_sub = Subscriber(self, Imu, '/imu')
         joint_sub = Subscriber(self, JointState, '/joint_states')
 
-        # Time synchronizer
+        # Time synchronizer - by choosing these two we are avoiding potential error of when t goes on but no cmd_vel is given.
         sync = ApproximateTimeSynchronizer([imu_sub, joint_sub], 10, 0.03)
         sync.registerCallback(self.synced_callback)
 
@@ -48,15 +56,16 @@ class Odometry_Tracking(Node):
         self.odom_pub = self.create_publisher(Odometry, '/ekf_odom', 10)
 
     def forgetting(self, dt):
+        # Made this into its own function per lab manual.
         a_v = math.pow(0.1, dt / self.tau_lin)
         a_w = math.pow(0.1, dt / self.tau_ang)
         return a_v, a_w
     
     def next_state(self, dt, a_v, a_w):
-        theta, x, y, v, w = self.state
-        u_v, u_w = self.u_v, self.u_w
+        theta, x, y, v, w = self.state # Order defined based on what is wanted in lab manual.
+        u_v, u_w = self.u_v, self.u_w # Initilaize values.
 
-        # first-order dynamic response from manual notes
+        # first-order dynamic response from manual notes - derivation can be found in document submission
         v_next = a_v * v + self.G_v * (1.0 - a_v) * u_v
         w_next = a_w * w + self.G_w * (1.0 - a_w) * u_w
 
@@ -73,13 +82,16 @@ class Odometry_Tracking(Node):
     def jacobians(self, dt, a_v, a_w, x_curr):
         theta, x, y, v, w = x_curr
 
+        # calculations can be found in notes
         Gx = np.array([
-            [1, 0, 0, 0, dt],
-            [-v * math.sin(theta) * dt, 1, 0, math.cos(theta) * dt, 0],
-            [ v * math.cos(theta) * dt, 0, 1, math.sin(theta) * dt, 0],
-            [0, 0, 0, a_v, 0],
-            [0, 0, 0, 0, a_w]
+            [1.0, 0.0, 0.0, 0.0, dt],
+            [-v * math.sin(theta) * dt, 1.0, 0.0, math.cos(theta) * dt, 0.0],
+            [ v * math.cos(theta) * dt, 0.0, 1.0, math.sin(theta) * dt, 0.0],
+            [0.0, 0.0, 0.0, a_v, 0.0],
+            [0.0, 0.0, 0.0, 0.0, a_w]
         ])
+
+        # Note Gu = B matrix - made this way to remember
 
         Gu = np.array([
             [0.0, 0.0],
@@ -93,10 +105,11 @@ class Odometry_Tracking(Node):
     
     def extract_measurements(self, imu_msg, joint_msg):
         # Wheel parameters - given in manual
-        r = 0.033       # 33 mm = 0.033 m
-        R = 0.07175     # 71.75 mm = 0.07175 m
+        r_w = 0.033       # 33 mm = 0.033 m
+        R_w = 0.07175     # 71.75 mm = 0.07175 m
 
         w_r = w_l = 0.0
+        # after exploring topic, found we need to filter by name first.
         if joint_msg.velocity:
             for name, vel in zip(joint_msg.name, joint_msg.velocity):
                 if 'wheel_right' in name:
@@ -108,8 +121,8 @@ class Odometry_Tracking(Node):
 
         # Measurement matrix (C)
         C = np.array([
-            [0.0, 0.0, 0.0, 1.0 / r,  R / r],
-            [0.0, 0.0, 0.0, 1.0 / r, -R / r],
+            [0.0, 0.0, 0.0, 1.0 / r_w,  R_w / r_w],
+            [0.0, 0.0, 0.0, 1.0 / r_w, -R_w / r_w],
             [0.0, 0.0, 0.0, 0.0, 1.0]
         ], dtype=float)
 
@@ -119,26 +132,32 @@ class Odometry_Tracking(Node):
         return z, C, R
 
     def cmd_callback(self, msg):
+        # Update here with what was given as input.
         self.u_v = msg.linear.x
         self.u_w = msg.angular.z
 
     def synced_callback(self, imu_msg, joint_msg):
         
         # compute delta t # got 0.034s
+
+        # We want to choose the one that takes the longest time - since intuitively the faster one would have already arrived
+        # Found that we needed to actually make it into a float point since these are headers and are split into secs and nano.
         t = max(
             imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec * 1e-9,
             joint_msg.header.stamp.sec + joint_msg.header.stamp.nanosec * 1e-9,
         )
-
+        
+        # Trial and error - was failing when we didnt consider for the fact that there is no time in the first callback.
         if self.last_time is None:
             self.last_time = t
             return
         
+        # calculate duration between last and current update - this becomes our dt - found it was pretty much consistent - 0.034s
         dt = t - self.last_time
         self.last_time = t
 
         # We moved this to the measurment extraction since it is needed there but will 
-        # also keep it here to make sure we can see what the outputs are before the filter
+        # also keep it here to make sure we can see what the outputs are before the filter - so basically for sanity check.
         w_r = w_l = 0.0
         if joint_msg.velocity:
             for name, vel in zip(joint_msg.name, joint_msg.velocity):
@@ -155,10 +174,14 @@ class Odometry_Tracking(Node):
         )
 
         # Prediction - Now lets get our predicted results from the functions defined above
+        # Bring in all parameters from above functions.
         a_v, a_w = self.forgetting(dt)
         Gx, Gu = self.jacobians(dt, a_v, a_w, self.state)
+
+        # calculate the hat_covariance
         self.state_covariance = Gx @ self.state_covariance @ Gx.T + Gu @ self.input_covariance @ Gu.T
 
+        # Next, we get the n state.
         x_pred, theta_next, x_next, y_next, v_next, w_next = self.next_state(dt, a_v, a_w)
         self.state = x_pred
 
@@ -166,7 +189,7 @@ class Odometry_Tracking(Node):
         z, C, R = self.extract_measurements(imu_msg, joint_msg)
 
         # EKF Innovation / Correction
-        K = self.state_covariance @ C.T @ np.linalg.inv(C @ self.state_covariance @ C.T + R)
+        K = self.state_covariance @ C.T @ np.linalg.inv((C @ self.state_covariance @ C.T) + R)
         self.state = self.state + K @ (z - C @ self.state)
         self.state_covariance = (np.eye(5) - K @ C) @ self.state_covariance
         
@@ -189,19 +212,20 @@ class Odometry_Tracking(Node):
         odom_msg.pose.pose.position.z = 0.0
 
         # Take consideration of the quaternions - publish
+        # Reference for hte formula usage of getting yaw to quternion - https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
         qz = math.sin(self.state[0]/2.0)
         qw = math.cos(self.state[0]/2.0)
         odom_msg.pose.pose.orientation = Quaternion(x =0.0, y=0.0, z=qz, w=qw)
 
         # Now we have to map 5x5 state to 6x6 expectation
         # First pose
-        odom_msg.pose.covariance = [0.0] * 36
+        odom_msg.pose.covariance = [0.0] * 36 # create covariance - make everything 0 at first since we are only giving it 3 values.
         diag_covariances = np.diag(self.state_covariance)
-        odom_msg.pose.covariance[0] = diag_covariances[1]
-        odom_msg.pose.covariance[7] = diag_covariances[2]
-        odom_msg.pose.covariance[35] = diag_covariances[0]
+        odom_msg.pose.covariance[0] = diag_covariances[1] # X covariance
+        odom_msg.pose.covariance[7] = diag_covariances[2] # Y covariance
+        odom_msg.pose.covariance[35] = diag_covariances[0] # theta covariance - just yaw
 
-        # Next twist
+        # Next twist - same reasoning as above.
         odom_msg.twist.twist.linear.x = self.state[3]
         odom_msg.twist.twist.angular.z = self.state[4]
         odom_msg.twist.covariance = [0.0]*36
--- prob_rob_labs/src/StateExtraction/StateExtraction.py
+++ prob_rob_labs/src/StateExtraction/StateExtraction.py
@@ -10,13 +10,14 @@ class StateExtraction(Node):
     def __init__(self):
         super().__init__('StateExtraction')
         
-        # Subscriber
+        # Subscriber for Link States
         self.create_subscription(LinkStates,'/gazebo/link_states', self.feature_callback, 10)
         
-        # Parameter for frame of reference
+        # Parameter for frame of reference - made default odom for the sake of this lab
         self.declare_parameter('frame_of_reference', 'odom')
 
-        # Publishers
+        # Publishers - both for twist and Pose
+        # For future reference: https://docs.ros.org/en/noetic/api/geometry_msgs/html/msg/Twist.html
         self.publisher_pose = self.create_publisher(PoseStamped, '/tb3/ground_truth/pose', 10)
         self.publisher_twist = self.create_publisher(TwistStamped, '/tb3/ground_truth/twist', 10)
 
@@ -30,24 +31,24 @@ class StateExtraction(Node):
 
     def feature_callback(self, msg):
         reference_frame = self.get_parameter('frame_of_reference').get_parameter_value().string_value
-        footprintIndex = msg.name.index('waffle_pi::base_footprint')
+        footprintIndex = msg.name.index('waffle_pi::base_footprint') # To get the right linke of interest - robots body
        
         # Check that everything is being recieved
         self.log.info('Received link_states message')
         
         # First, extract pose and then publish it.
         pose_msg = PoseStamped()
-        pose_msg.header.stamp = self.get_clock().now().to_msg()
-        pose_msg.header.frame_id = reference_frame
-        pose_msg.pose = msg.pose[footprintIndex]
+        pose_msg.header.stamp = self.get_clock().now().to_msg() # Get time
+        pose_msg.header.frame_id = reference_frame # Get reference frame
+        pose_msg.pose = msg.pose[footprintIndex] # and finally the pose of the link of interest - extraction here
 
         # Now we do the same for the twist
         twist_msg = TwistStamped()
         twist_msg.header.stamp = pose_msg.header.stamp
         twist_msg.header.frame_id = reference_frame
-        twist_msg.twist = msg.twist[footprintIndex]
+        twist_msg.twist = msg.twist[footprintIndex] # extraction of the twist - so velocity
 
-        # Publish the messages
+        # Publish the full message that we have extracted.
         self.publisher_pose.publish(pose_msg)
         self.publisher_twist.publish(twist_msg)
         self.log.info('Published ground truth pose and twist.')
