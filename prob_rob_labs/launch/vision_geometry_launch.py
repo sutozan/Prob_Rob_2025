0a05a2e7d7e7d0fb01351f8ab6837e72076635fa
Suheyla Tozan <suheylatozan02@gmail.com>
2025-11-03 02:49:11 -0500

    Lab 5, A1 - Height/Vertical Axis Symmetry Calculation Node

---
+++ prob_rob_labs/launch/vision_geometry_launch.py
@@ -0,0 +1,25 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true',
+                              description='set to true for simulation'),
+        DeclareLaunchArgument('color', default_value='magenta',
+                              description='set color for topic name'),
+        DeclareLaunchArgument('manual', default_value='false',
+                              description='setting up mode for code testing'),
+        Node(
+            package='prob_rob_labs',
+            executable='vision_geometry',
+            name='vision_geometry',
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
+                        {'color': LaunchConfiguration('color')},
+                        {'manual': LaunchConfiguration('manual')}]
+        )
+    ])
