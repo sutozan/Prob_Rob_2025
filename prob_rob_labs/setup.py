d442f5095604f9395631bad28a762f82538c92b2
Suheyla Tozan <suheylatozan02@gmail.com>
2025-11-06 03:43:02 -0500

    Lab5 A2, Boilerplate code

--- prob_rob_labs/setup.py
+++ prob_rob_labs/setup.py
@@ -33,6 +33,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'measurment_calculations = measurment_calculations.measurment_calculations:main',
             'vision_geometry = vision_geometry.vision_geometry:main',
             'euclidean_distance_determiner = euclidean_distance_determiner.euclidean_distance_determiner:main',
             'Odometry_Tracking = Odometry_Tracking.Odometry_Tracking:main',
